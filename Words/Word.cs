735fd48ee9fc421122d0ca989970acf1fdc1e229
Ditte <Ditte@DESKTOP-QORFADN>
2017-10-30 11:37:04 +0100

    Words project added with class Word, with empty method bodies for FindSpecificCharacters(), CountSpecificCharacters() and CountAllCharacters()

---
+++ Words/Word.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    public class Word
+    {
+        public string Text;
+
+        public Word(string text)
+        {
+            Text = text;
+        }
+
+        public bool FindSpecificCharacter(string character)
+        {
+
+        }
+
+        public int CountSpecificCharacters(Word word)
+        {
+
+        }
+
+        public int CountAllCharacters(Word word)
+        {
+
+        }
+
+    }
+}
